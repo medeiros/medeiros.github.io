f1ae45aff5b2c1e0b22cfc3e71f18c07aaa43698
medeiros <daniel@arneam.com>
2024-06-07 06:43:04 -0300

    Improve jekyll execution by running draft mode

--- start.sh
+++ start.sh
@@ -4,4 +4,5 @@ export LC_ALL=en_US.UTF-8
 export LANG=en_US.UTF-8
 export LANGUAGE=en_US.UTF-8
 
-bundle exec jekyll serve --future
+bundle exec jekyll serve --livereload --drafts
+#bundle exec jekyll serve --future
